0952e4f5ba8c5a5abab8c566acf45af0a2ae962e
Pavithra <pavithra466.K@gmail.com>
2018-11-28 16:26:38 -0800

    Update
    Deleted comments in showActionIndicator Button Action

--- NewProject/ViewController.swift
+++ NewProject/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
-
+import MKSpinner
+import SwiftHEXColors
+import SVProgressHUD
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var hexaColorLabel: UILabel!
+    
+    @IBAction func showActionIndicator(_ sender: Any) {
+       
+      MKNSpinner.show("Connecting Satellite.....")
+        
+   }
+    
+    @IBAction func showDismissIndicator(_ sender: Any) {
+        
+        MKNSpinner.hide()
+    }
+    
+    
+    @IBAction func showProgressAction(_ sender: Any) {
+        //[SVProgressHUD(), show] as [Any];
+        SVProgressHUD.show()
+    }
+    
+    @IBAction func shoeDismissProgress(_ sender: Any) {
+        //[SVProgressHUD(), dismiss] as [Any];
+        SVProgressHUD.dismiss()
+    }
+    
+    
+    
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var inputLabel: UILabel!
+    
+    @IBAction func updateButtonClicked(_ sender: Any) {
+        self.inputLabel.text = inputTextField.text
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //UIColor(hexString: "#ff8942")
+        self.hexaColorLabel.text = "hexaColorLabel"
+        hexaColorLabel.textColor = UIColor(hexString: "#ff8942")
+        
     }
 
     override func didReceiveMemoryWarning() {
